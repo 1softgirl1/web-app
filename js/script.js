5be7dba3759a0f2a0fa64fa89ae5e9c8d5d8a054
lidiya <karavaeva.lidaa@yandex.ru>
2025-09-17 10:53:17 +0700

    add skill carousel using OwlCarousel2

--- js/script.js
+++ js/script.js
@@ -124,4 +124,29 @@ $(document).ready(function() {
             portfolioContainer.append(projectCard);
         });
     }
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        },
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+
+
+
+
 });
