76a3f30ae7f66f2627e8f92dcd115b4f967c9c6a
lidiya <karavaeva.lidaa@yandex.ru>
2025-10-05 14:08:30 +0700

    edit arrows

--- js/script.js
+++ js/script.js
@@ -124,7 +124,12 @@ $(document).ready(function() {
         },
         autoplay: true,
         autoplayTimeout: 3000,
-        autoplayHoverPause: true
+        autoplayHoverPause: true,
+
+        navText: [
+            "<img src='img/left-arrow.png' alt='Previous'>",
+            "<img src='img/right-arrow.png' alt='Next'>"
+        ]
     });
 
     //Кнопка «Вверх» — плавный скролл к началу страницы
